1c3cf6df9bd6c9f83abc41969f7a20b2b8561906
Binar Aris <binar@outlook.com>
2024-10-19 07:40:53 +0700

    penambahan icon pada calon

--- app/src/main/java/com/binararis/pilketos/AdapterPerhitungan.kt
+++ app/src/main/java/com/binararis/pilketos/AdapterPerhitungan.kt
@@ -17,7 +17,8 @@ import com.google.firebase.database.ValueEventListener
 class AdapterPerhitungan
     (private val listHasil: ArrayList<ModelCalon>) :
     RecyclerView.Adapter<AdapterPerhitungan.MyViewHolder>() {
-        lateinit var appContext:Context
+    lateinit var appContext: Context
+
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvNomorCalon: TextView = itemView.findViewById(R.id.tvCalon)
         val tvNamaCalon: TextView = itemView.findViewById(R.id.tvNamaCalon)
@@ -37,12 +38,21 @@ class AdapterPerhitungan
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val item = listHasil[position]
-        holder.tvNomorCalon.text="No Calon: "+item.idCalon
-        holder.tvNamaCalon.text=item.nama
-        holder.ivFotoCalon.setImageResource(R.drawable.ic_launcher_foreground)
+        holder.tvNomorCalon.text = "No Calon: " + item.idCalon
+        holder.tvNamaCalon.text = item.nama
+        if (item.idCalon.equals("1")) {
+            holder.ivFotoCalon.setImageResource(R.drawable.baseline_person_24)
+        }else if(item.idCalon.equals("2")){
+            holder.ivFotoCalon.setImageResource(R.drawable.baseline_person_2_24)
+        }else if(item.idCalon.equals("3")){
+            holder.ivFotoCalon.setImageResource(R.drawable.baseline_person_3_24)
+        }else{
+            holder.ivFotoCalon.setImageResource(R.drawable.baseline_no_accounts_24)
+        }
         holder.ivFotoCalon.setBackgroundResource(R.drawable.ic_launcher_background)
         //mencari jumlah data
-        val database = FirebaseDatabase.getInstance("https://binarpilketos-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        val database =
+            FirebaseDatabase.getInstance("https://binarpilketos-default-rtdb.asia-southeast1.firebasedatabase.app/")
         val myRef = database.getReference("perhitungan")
         val query = myRef.orderByChild("idCalon").equalTo(item.idCalon)
         query.addValueEventListener(object : ValueEventListener {
